e832b835705c45759857002dcdda8d3fdc3c2855
Swork91 <chicagomike999@gmail.com>
2018-04-09 12:14:40 -0400

    factorials

--- src/ComputerFactorial.java
+++ src/ComputerFactorial.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
 
 public class ComputerFactorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int n = 0;
+		while (n != -1) {
+			System.out.println("Enter a number: ");
+			n = input.nextInt();
+			
+			if (n>=0) {
+				System.out.println("Factorial of "+n+" is "+factorial(n));
+			}
+			else {
+				System.out.println("\ngoodbye");
+			}
+
+		}
+
 
 	}
 
+	private static long factorial(int n) {
+		if (n<2) {
+			return 1;
+		}
+		else {
+			return n*factorial(n-1);
+		}
+	}
+
 }
+
